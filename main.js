66d114cb52dc0b102c1f48cf0fc54065d87fabd8
Kollos2k <kollos@vorsicht-bissig.de>
2023-12-27 11:46:28 +0100

    main.js updated

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ class visShelly extends utils.Adapter {
 			name: "vis-shelly",
 		});
 		this.on("ready", this.onReady.bind(this));
-		//this.on("objectChange", this.onObjectChange.bind(this));
+		this.on("objectChange", this.onObjectChange.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
 		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
@@ -29,90 +29,39 @@ class visShelly extends utils.Adapter {
 	 */
 	async onReady() {
 		this.setState("info.connection", false, true);
+		await this.subscribeForeignObjectsAsync("shelly.*");
 		// Initialize your adapter here
 
-//      this.log.info("config Stripes: " + this.config.Stripes);
-    //   this.log.info("config Background: " + this.config.Background);
-//      this.log.info("config Radius: " + this.config.Radius);
-//      this.log.info("config Info: " + this.config.Info);
+
+		// await this.setForeignObjectAsync("vis-shelly.0", {
+		// 	type: "meta",
+		// 	common: {
+		// 		"name": {
+		// 		  "en": "Meta storage for user files1",
+		// 		  "de": "Meta-Speicher für Benutzerdateien",
+		// 		  "ru": "Meta Storage для файлов пользователей",
+		// 		  "pt": "Meta de armazenamento para arquivos de usuário",
+		// 		  "nl": "Meta opslag voor gebruikersbestanden",
+		// 		  "fr": "Stockage Meta pour les fichiers utilisateur",
+		// 		  "it": "Meta storage per i file utente",
+		// 		  "es": "Meta almacenamiento para archivos de usuario",
+		// 		  "pl": "Meta storage for user files",
+		// 		  "uk": "Зберігання мета для файлів користувачів",
+		// 		  "zh-cn": "用户档案的储存"
+		// 		},
+		// 		"type": "meta.user"
+		// 	},
+		// 	native: {},
+		// });
+
 		this.log.info("config option1: " + this.config.option1);
 		this.log.info("config option2: " + this.config.option2);
+		this.updateDeviceList();
+		this.setState("info.connection", true, true);
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		
-        await this.setObjectNotExistsAsync("CSS.Button2", {
-			type: "state",
-			common: {
-				name: "Button-Color",
-				type: "string",
-				role: "shelly.color",
-				read: true,
-				write: true,
-			},
-			native: {},
-        });
-        await this.setObjectNotExistsAsync("CSS.Active", {
-			type: "state",
-			common: {
-				name: "Button-Active-Color",
-				type: "string",
-				role: "shelly.color",
-				read: true,
-				write: true,
-			},
-            native: {},
-        });
-        await this.setObjectNotExistsAsync("CSS.Text", {
-			type: "state",
-			common: {
-				name: "Text-Color",
-				type: "string",
-				role: "shelly.color",
-				read: true,
-				write: true,
-			},
-			native: {},
-       });
-	   await this.setObjectNotExistsAsync("shellyObjects.count", {
-		   type: "state",
-		   common: {
-			   name: "Count Shelly Objects",
-			   type: "string",
-			   role: "shelly.count",
-			   read: true,
-			   write: true,
-		   },
-		   native: {},
-	   });
-        
-		// in this template all states changes inside the adapters namespace are subscribed
-        // this.subscribeStates("CSS.Button");
-        // this.subscribeStates("CSS.Active");
-        // this.subscribeStates("CSS.Text");
-        // this.subscribeStates("shellyObjects.count");
+	}
 
-
-		/*
-		setState examples
-		you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		//await this.setStateAsync("testVariable", true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		// if(await this.getStateAsync("CSS.Button") == null)
-        // 	await this.setStateAsync("CSS.Button", { val: "#333333", ack: true });
-		// if(await this.getStateAsync("CSS.Active") == null)
-        // 	await this.setStateAsync("CSS.Active", { val: "#455618", ack: true });
-		// if(await this.getStateAsync("CSS.Text") == null)
-        // 	await this.setStateAsync("CSS.Text", { val: "#C7C7C7", ack: true });
-
-		//var shellyClass = await this.getForeignObjectsAsync("shelly.0.*");
+	async updateDeviceList(){
 		var shellyDevices = await this.getForeignObjectsAsync("shelly.*","device");
 		var keysDevices=Object.keys(shellyDevices);	
 	   	var devJSON=[]
@@ -158,11 +107,13 @@ class visShelly extends utils.Adapter {
 				native: {},
 			});
 
-			let typeState=await this.getForeignStateAsync(deviceID+".type");
-			this.setState("devices."+deviceName+".type", { val: typeState.val, ack: true });
-			devJSON.push({"stateId":deviceID,"id":deviceName,"type":typeState.val});
 			let relayCount=1;
-			if(typeState.val=="shellyplus2pm")relayCount=2
+			let typeState=await this.getForeignStateAsync(deviceID+".type");
+			if(typeState!=null){
+				this.setState("devices."+deviceName+".type", { val: typeState.val, ack: true });
+				devJSON.push({"stateId":deviceID,"id":deviceName,"type":typeState.val});
+				if(typeState.val=="shellyplus2pm")relayCount=2
+			}
 
 			for(let i=0;i<relayCount;i++){
 
@@ -185,43 +136,9 @@ class visShelly extends utils.Adapter {
 					native: {},
 				},()=>{});	
 			}
-			// this.createAndSetState(deviceID,"devices."+deviceName+".deviceID",deviceName+".deviceID","device.id");
-			// this.createAndSetState(deviceID+".name","devices."+deviceName+".nameID",deviceName+".nameID","name.id");
-			// this.createAndSetStateIfExists(deviceID+".Relay0.Switch","devices."+deviceName+".switchID",deviceName+".switchID","switch.id");
-			// this.createAndSetStateIfExists(deviceID+".lights.Switch","devices."+deviceName+".switchID",deviceName+".switchID","switch.id");
-			// this.createAndSetStateIfExists(deviceID+".Relay0.Power","devices."+deviceName+".powerID",deviceName+".powerID","power.id");
-			// this.createAndSetStateIfExists(deviceID+".lights.Power","devices."+deviceName+".powerID",deviceName+".powerID","power.id");
-			// this.createAndSetStateIfExists(deviceID+".lights.brightness","devices."+deviceName+".brightnessID",deviceName+".brightnessID","brightness.id");
-
-
-			// for(let key of keysObject){
-			// 	//let subKey=key.substring(key.lastIndexOf("."),key.length);
-			// 	//if(key!="info" && key!="ble"){
-			// 	//if(key=="info"){
-			// 	//	await this.setStateAsync("shellyObjects.count", { val: key.length, ack: true });
-			// 	//	break;
-			// 	//} else {
-			// 	//	await this.setStateAsync("shellyObjects.count", { val: "", ack: true });
-			// 	//}
-			// 	//if(subKey!="enums"&&subKey!="acl")
-			// 	//await this.setStateAsync("shellyObjects.count", { val: subKey, ack: true });
-			// }
 		}	
         await this.setStateAsync("devices.ids", { val: JSON.stringify(devJSON), ack: true });
-		//var shellyClass = await this.getForeignStateAsync("shelly").val;
-
-        await this.setStateAsync("shellyObjects.count", { val: shellyClass, ack: true });
-		//await this.setStateAsync("shellyObjects.count", { val: keysInstance.length, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		//await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		//let result = await this.checkPasswordAsync("admin", "ioBroker");
-		//this.log.info("check user admin pw ioBroker: " + result);
-
-		//result = await this.checkGroupAsync("admin", "admin");
-		//this.log.info("check group user admin group admin: " + result);
+		this.log.info("Devices updated");
 	}
 
 	/**
@@ -230,6 +147,7 @@ class visShelly extends utils.Adapter {
 	 */
 	onUnload(callback) {
 		try {
+			this.setState("info.connection", false, true);
 			this.log.info("cleaned everything up...");
 			callback();
 		} catch (e) {
@@ -245,10 +163,11 @@ class visShelly extends utils.Adapter {
 	onObjectChange(id, obj) {
 		if (obj) {
 			// The object was changed
-			this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+			if(obj.type=="device")this.updateDeviceList();
+			// this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
 		} else {
 			// The object was deleted
-			this.log.info(`object ${id} deleted`);
+			// this.log.info(`object ${id} deleted`);
 		}
 	}
 
